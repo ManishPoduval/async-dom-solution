d3d3821254f75b7da3ba68eddc3da194beb4c8de
Manish Poduval <manishpoduval@hotmail.com>
2021-01-20 20:18:07 +0530

    solution

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ let startBtn = document.querySelector('#start')
 let stopBtn = document.querySelector('#stop')
 let timeDOM = document.querySelector('#timer')
 
-
+let clock = new Timer()
+console.log(clock)
 // STEP 3: Create an object from the timer class
 
 function updateTime(){
    // update the time DOM here
    // console.log('Here')
-    
+
+   timeDOM.innerHTML = clock.time
+   console.log('Update method', clock.time)
 }
 
 
 startBtn.addEventListener('click', () => {
     // Call the timer start method
+    clock.start(updateTime)
+
 })
 
 
 stopBtn.addEventListener('click', () => {
-    // Call the timer stop method
+    clock.stop()
 })
